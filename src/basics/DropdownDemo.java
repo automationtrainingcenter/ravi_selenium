5d5e8b149f77526bca2d8601ccff48573ed05cfa
automation <atcsurya@gmail.com>
2019-07-05 08:37:28 +0530

    dropdownlist and listbox

---
+++ src/basics/DropdownDemo.java
@@ -0,0 +1,80 @@
+package basics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropdownDemo {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.manage().window().maximize();
+
+		/*
+		 * To automate dropdown list or list box selenium provides Select class 1.
+		 * Create an Object of Select class by passing WebElement as argument to
+		 * constructor
+		 */
+		// locate date element
+		WebElement dateEle = driver.findElement(By.id("day"));
+		Select dateSelect = new Select(dateEle);
+		/*
+		 * to retrieve default selected option use getFirstSelectedOption
+		 */
+		String defaultDate = dateSelect.getFirstSelectedOption().getText();
+		System.out.println("default date is " + defaultDate);
+		/*
+		 * select an option based on the index number of option tag and index number
+		 * starts from 0 -- selectByIndex(int arg)
+		 */
+		dateSelect.selectByIndex(18);
+		Thread.sleep(2000);
+
+		// locate month element
+//		WebElement monthEle = driver.findElement(By.id("month"));
+//		Select monthSelect = new Select(monthEle);
+		Select monthSel = new Select(driver.findElement(By.id("month")));
+		
+		// retrieve default month
+		String defaultMonth = monthSel.getFirstSelectedOption().getText();
+		System.out.println("default month is " + defaultMonth);
+		
+		/*to retrieve all the options of a dropdown list or list box 
+		 * use getOptions() which will return a List of WebElements
+		 */
+		List<WebElement> options = monthSel.getOptions();
+		List<String> optionsText = new ArrayList<>();
+		for(WebElement option : options) {
+			optionsText.add(option.getText());
+		}
+		System.out.println(optionsText.contains("Apr"));
+		
+		/*
+		 * select an option based on value of the value attribute of option tag --
+		 * selectByValue(String arg)
+		 */
+		monthSel.selectByValue("11");
+		Thread.sleep(2000);
+
+		// locate year element
+		Select yearSelect = new Select(driver.findElement(By.id("year")));
+		// retrieve default year
+		String defaultYear = yearSelect.getFirstSelectedOption().getText();
+		System.out.println("default year is " + defaultYear);
+		/*
+		 * Select an option based on the inner text i.e. text in between opening
+		 * <option> and closing </option> tags -- selectByVistibleText(String arg)
+		 */
+		yearSelect.selectByVisibleText("2020");
+		Thread.sleep(2000);
+
+		driver.close();
+	}
+
+}
---
+++ src/basics/ListBoxDemo.java
@@ -0,0 +1,55 @@
+package basics;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ListBoxDemo {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://learn.letskodeit.com/p/practice");
+		driver.manage().window().maximize();
+
+		// locate list box
+		WebElement fruitsList = driver.findElement(By.id("multiple-select-example"));
+		// Create Select class object
+		Select fruitSelect = new Select(fruitsList);
+		// select an option based on index
+		fruitSelect.selectByIndex(2); // Peach
+		Thread.sleep(2000);
+		// slect an option based on value
+		fruitSelect.selectByValue("apple");
+		Thread.sleep(2000);
+
+		// retrieve all selectd options
+		List<WebElement> selectedOptions = fruitSelect.getAllSelectedOptions();
+		for (WebElement option : selectedOptions) {
+			System.out.println(option.getText());
+		}
+
+		// select an option based on visible text
+		fruitSelect.selectByVisibleText("Orange");
+		Thread.sleep(2000);
+		// deselect all selected options
+//		fruitSelect.deselectAll();
+//		Thread.sleep(2000);
+
+		// deselect an option based on index
+		fruitSelect.deselectByIndex(1); // Orange
+		Thread.sleep(2000);
+		// deselect an option based on value of the value attribute
+		fruitSelect.deselectByValue("peach");
+		Thread.sleep(2000);
+		// deselect an option based on visible text
+		fruitSelect.deselectByVisibleText("Apple");
+		Thread.sleep(2000);
+
+		driver.close();
+	}
+
+}
